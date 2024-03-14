315127127a9e5e505b26eacafa32af8f91f1a7be
HipsterBrown <headhipster@hipsterbrown.com>
2024-03-14 17:35:20 -0400

    refactor: adopt speech-service-api

--- src/stt_vosk/__main__.py
+++ src/stt_vosk/__main__.py
@@ -1,14 +1,14 @@
 import asyncio
 
 from viam.module.module import Module
-from viam.services.generic import Generic
 
+from speech_service_api import SpeechService
 from . import SttVosk
 
 
 async def main():
     module = Module.from_args()
-    module.add_model_from_registry(Generic.SUBTYPE, SttVosk.MODEL)
+    module.add_model_from_registry(SpeechService.SUBTYPE, SttVosk.MODEL)
     await module.start()
 
 
