e92a682ec96f894b55ad3364c20e6c822b0d218d
HipsterBrown <headhipster@hipsterbrown.com>
2025-04-24 12:15:46 -0400

    fix: add pyaudio hidden import to build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export PATH=$PATH:$HOME/.local/bin
 
-uv run pyinstaller --onefile --collect-all vosk --paths src src/main.py
+uv run pyinstaller --onefile --collect-all vosk --hidden-import pyaudio --paths src src/main.py
 tar -czvf dist/archive.tar.gz dist/main meta.json first_run.sh
