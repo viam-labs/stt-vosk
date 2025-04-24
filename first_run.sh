ae55060b16bdc04251dd518f1c9f4d2c1b651e9c
Matt Vella <mcvella@gmail.com>
2025-04-24 10:36:31 -0400

    add first_run.sh

---
+++ first_run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if command -v apt >/dev/null 2>&1; then
+  sudo apt install -qqy python3-pyaudio portaudio19-dev alsa-tools alsa-utils flac python3-dev clang ffmpeg
+fi
+
+if command -v brew >/dev/null 2>&1; then
+  brew install portaudio
+fi
