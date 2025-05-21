7c374fbedd936a42a6cc580f1e11a0a1963179ee
HipsterBrown <headhipster@hipsterbrown.com>
2025-05-21 18:09:39 -0400

    chore: add --fix-missing to first_run script

--- first_run.sh
+++ first_run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if command -v apt >/dev/null 2>&1; then
-  sudo apt install -qqy python3-pyaudio portaudio19-dev alsa-tools alsa-utils flac python3-dev clang ffmpeg
+  sudo apt install -qqy --fix-missing python3-pyaudio portaudio19-dev alsa-tools alsa-utils flac python3-dev clang ffmpeg
 fi
 
 if command -v brew >/dev/null 2>&1; then
